eb00a8f3a8c59f0965348f907da6e5415a064c92
Joel Brewer <joelbrewer01@gmail.com>
2014-11-10 11:25:13 -0600

    add integration test for adding a volunteer.

--- test/integration/signup_volunteer_test.rb
+++ test/integration/signup_volunteer_test.rb
@@ -8,6 +8,24 @@ class SignupVolunteerTest < ActionDispatch::IntegrationTest
     click_link(I18n.t('am_a_volunteer'))
 
     assert_equal(I18n.t('create_volunteer'), find(:css, 'input#new-user-submit').value)
+
+    # fill out volunteer form
+    volunteer = users(:volunteer)
+    fill_in('First name', :with => volunteer.first_name)
+    fill_in('Last name', :with => volunteer.last_name)
+    fill_in('City', :with => volunteer.city)
+    fill_in('Email', :with => volunteer.email)
+    check(focuses(:community).name)
+    select(professions(:agriculture).name)
+    fill_in('Password', :with => 'helloworld')
+    fill_in('Password confirmation', :with => 'helloworld')
+
+    assert_difference 'Volunteer.count', 1 do
+      click_button('Signup')
+    end
+
+    @volunteer = Volunteer.last
+    
   end
 
 end
