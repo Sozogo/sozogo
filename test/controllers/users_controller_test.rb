660a44c5ca8ab8f4bb01bc9b6e67be5077b5f38e
Joel Brewer <joelbrewer01@gmail.com>
2014-12-08 21:15:06 -0600

    add test to check for acceptance of terms

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,4 +15,18 @@ class UsersControllerTest < ActionController::TestCase
     get(:new, {'type' => "Organization"})
     assert_template %r{\Ausers/organizations/new\Z}
   end
+
+  test "should create organization" do
+    organization = users(:cbc)
+    assert_difference('User.count') do
+      post :create, organization: { email: organization.email, password: "helloworld", organization_type: organization.organization_type, organization_name: organization.organization_name, organization_contact_name: organization.organization_contact_name, address: organization.address, statement_of_beliefs: true }, "type" => "Organization"
+    end
+  end
+
+  test "should not create organization if terms are not accepted" do
+    organization = users(:cbc)
+    assert_no_difference('User.count') do
+      post :create, organization: { email: organization.email, organzation_type: organization.type, organization_name: organization.organization_name, organization_contact_name: organization.organization_contact_name, address: organization.address }, "type" => "Organization"
+    end
+  end
 end
