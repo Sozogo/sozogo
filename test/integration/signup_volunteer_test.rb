95fdc8154475a83a013aa88653f19d6a97094018
Joel Brewer <joelbrewer01@gmail.com>
2015-03-13 20:16:18 -0500

    fix test suite after making changes to user registration pages

--- test/integration/signup_volunteer_test.rb
+++ test/integration/signup_volunteer_test.rb
@@ -10,19 +10,19 @@ class SignupVolunteerTest < ActionDispatch::IntegrationTest
     assert_equal(I18n.t('create_volunteer'), find(:css, 'input#new-user-submit').value)
 
     # fill out volunteer form
-    fill_in('First name', :with => "Joel")
-    fill_in('Last name', :with => "Brewer")
-    fill_in('City', :with => "Nashville")
-    fill_in('Email', :with => "jb@brewerdigital.com")
-    fill_in('Zipcode', :with => "37211")
+    fill_in('volunteer_first_name', :with => "Joel")
+    fill_in('volunteer_last_name', :with => "Brewer")
+    fill_in('volunteer_city', :with => "Nashville")
+    fill_in('volunteer_email', :with => "jb@brewerdigital.com")
+    fill_in('volunteer_zipcode', :with => "37211")
     select('Tennessee', :from => 'volunteer_state')
     select('01', :from => 'volunteer_birth_day')
     select('01', :from => 'volunteer_birth_month')
     select('1991', :from => 'volunteer_birth_year')
     select(focuses(:community).name)
     select(professions(:agriculture).name)
-    fill_in('Password', :with => 'helloworld')
-    fill_in('Password confirmation', :with => 'helloworld')
+    fill_in('volunteer_password', :with => 'helloworld')
+    fill_in('volunteer_password_confirmation', :with => 'helloworld')
 
     assert_difference 'Volunteer.count', 1 do
       click_button('Signup')
